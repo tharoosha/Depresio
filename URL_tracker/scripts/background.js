08de810f0cd351add03745f1514ea1aa63ece31a
tharoosha <tharooshavihidun@gmail.com>
2023-05-26 11:13:47 +0530

    Updated

--- URL_tracker/scripts/background.js
+++ URL_tracker/scripts/background.js
@@ -1,40 +1,71 @@
 // background.js
 try{
   // Track URLs and time spent on each page
-  let startTime = new Date().getTime();
+  let startTime = 0;
   let previousUrl = '';
   let timeSpent = 0;
+  let elapsedTime = 0; 
+  // let Vtitle = '';
 
-  function trackPageVisit(tab) {
-    const currentTime = new Date().getTime();
-    const elapsedTime = Math.round((currentTime - startTime) / 1000); // Calculate elapsed time in seconds
-    const currentUrl = tab.url;
 
-    // Log the previous URL and the time spent on that page
-    console.log(`Visited URL: ${previousUrl}`);
-    console.log(`Time spent on the previous page: ${elapsedTime} seconds`);
+  // // YouTube API Key
+  // const apiKey = 'AIzaSyDKFRkk0HQWBIXQqQjaDW9-Fz9opaFkZys';
 
-    // Update the previous URL and start time for the next page
-    previousUrl = currentUrl;
-    timeSpent = elapsedTime;
+  // // Function to extract content name from YouTube URL
+  // function getContentName(url) {
+  //   // Extract video ID from YouTube URL
+  //   const videoId = url.match(/(?<=v=|v\/|embed\/|youtu.be\/|\/v\/|\/e\/|watch\?v=|youtube.com\/embed\/)([^#\&\?]*).*/i)[1];
 
-    // Update the start time for the next page
-    startTime = currentTime;
-  }
+  //   // Make API request to get video details
+  //   fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&key=${apiKey}`)
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       // Extract video title from the response
+  //       Vtitle = data.items[0].snippet.title;
+        
+  //       // Use the video title as needed
+  //       console.log('Video Title:', Vtitle);
+  //     })
+  //     .catch(error => {
+  //       console.log('Error:', error);
+  //     });
+
+  // }
+
+  // Example usage
+  // const youtubeURL = 'https://www.youtube.com/watch?v=VIDEO_ID';
+  // getContentName(youtubeURL);
+
+
 
   // Add a listener to track page transitions
   chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    if (changeInfo.url && changeInfo.url.startsWith('http')) {
-      trackPageVisit(tab);
+    if (changeInfo.url && (changeInfo.url.startsWith('http')) || changeInfo.url.startsWith('https')) {
+      startTime = new Date().getTime();
+      // trackPageVisit(tab);
+      previousUrl = tab.url;
+      // Vtitle = getContentName(previousUrl);
+
+      // Update the timer every second
+      setInterval(() => {
+        if (startTime) {
+          // Calculate the elapsed time in seconds
+          elapsedTime = Math.round((new Date().getTime() - startTime) / 1000);
+          // timerElement.innerText = `Page opened: ${elapsedTime} seconds ago`;
+        }
+      }, 1000);
+
     }
   });
+
 
   // Add a listener to respond to popup requests for previous URL and time spent
   chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.action === 'getPreviousData') {
       sendResponse({
         previousUrl: previousUrl,
-        timeSpent: timeSpent,
+        timeSpent: elapsedTime,
+        // title: Vtitle,
       });
     }
   });
--- URL_tracker/scripts/popup.js
+++ URL_tracker/scripts/popup.js
@@ -2,15 +2,22 @@
 
 // Function to retrieve data from the background script
 chrome.runtime.sendMessage({ action: 'getPreviousData' }, function(response) {
-    const previousUrlElement = document.getElementById('previousUrl');
-    const timeSpentElement = document.getElementById('timeSpent');
-  
-    if (response) {
-      previousUrlElement.textContent = response.previousUrl;
-      timeSpentElement.textContent = response.timeSpent;
-    } else {
-      previousUrlElement.textContent = 'N/A';
-      timeSpentElement.textContent = 'N/A';
-    }
-  });
-  
+  const previousUrlElement = document.getElementById('previousUrl');
+  const timeSpentElement = document.getElementById('timeSpent');
+  // const videotitleElement = document.getElementById('Vtitle');
+
+  if (response) {
+    previousUrlElement.textContent = response.previousUrl;
+    timeSpentElement.textContent = response.timeSpent;
+    // videotitleElement.textContent = response.Vtitle;
+  } else {
+    previousUrlElement.textContent = 'N/A';
+    timeSpentElement.textContent = 'N/A';
+    // videotitleElement.textContent = 'N/A';
+  }
+
+
+  if (response.timeSpent >= 20){
+    alert(`you are spending too much on ${response.previousUrl}`);
+  }
+});
